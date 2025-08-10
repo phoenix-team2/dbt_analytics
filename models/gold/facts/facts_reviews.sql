602f0fa2c037cee470c1967b502fd2c9271c5fff
David Shegun <davshegs@gmail.com>
2025-08-10 14:38:29 +0000

    fact_reviews created

---
+++ models/gold/facts/facts_reviews.sql
@@ -0,0 +1,18 @@
+with reviews as (
+    select * from {{ref ('stg_reviews')}}
+)
+
+select 
+r.review_id,
+r.product_id,
+cus.customer_id,
+r.review_title,
+r.review_date,
+r.rating,
+r.total_votes,
+r.helpful_votes,
+r.sentiment_score,
+r.verified_purchase
+from reviews as r
+join {{ref('dim_customers')}} as cus
+on r.customer_id = cus.customer_id
